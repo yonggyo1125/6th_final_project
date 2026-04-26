8536b446616182f5262dd2de5a1ec8e446539bd2
yonggyo1981 <yonggyo00@kakao.com>
2026-04-26 16:47:01 +0900

    .

---
+++ monitoring-server/src/test/java/org/sparta/monitoringserver/MonitoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.monitoringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
