d31d19a5cec26390afed1818fa91fa614eff0b2e
yonggyo1981 <yonggyo00@kakao.com>
2026-04-27 21:34:24 +0900

    .

---
+++ monitoring-server/src/main/java/org/sparta/monitoringserver/operation/presentation/OperationController.java
@@ -0,0 +1,33 @@
+package org.sparta.monitoringserver.operation.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/operation")
+public class OperationController {
+    private final ObjectMapper objectMapper;
+
+    @PostMapping(value = "/interactive", consumes = "application/x-www-form-urlencoded")
+    public Mono<Void> handleInteractive(@RequestParam("payload") String payloadString) {
+
+        return Mono.fromCallable(() -> objectMapper.readTree(payloadString))
+                .flatMap(payload -> {
+                    String actionId = payload.at("/actions/0/action_id").asText();
+                    String serviceName = payload.at("/actions/0/value").asText();
+                    String responseUrl = payload.path("response_url").asText();
+
+                    log.info("슬랙 액션 수신: actionId={}, service={}", actionId, serviceName);
+                    return Mono.empty();
+                });
+    }
+}
+
