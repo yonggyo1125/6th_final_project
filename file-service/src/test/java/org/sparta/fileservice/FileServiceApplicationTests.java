80d86bcbc6cb48eb65bd8f97d7264584a16a176b
yonggyo1981 <yonggyo00@kakao.com>
2026-05-04 11:15:53 +0900

    .

---
+++ file-service/src/test/java/org/sparta/fileservice/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
