aebeffdfbb326fd5d53c5468dbdfe912b4d0e578
yonggyo1981 <yonggyo00@kakao.com>
2026-05-04 17:16:37 +0900

    .

---
+++ file-service/src/main/java/org/sparta/fileservice/infrastructure/storage/LocalFileDownloader.java
@@ -0,0 +1,28 @@
+package org.sparta.fileservice.infrastructure.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.sparta.fileservice.domain.FileInfo;
+import org.sparta.fileservice.domain.service.FileDownloadContent;
+import org.sparta.fileservice.domain.service.FileDownloader;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(
+        name = "file.storage.type",
+        havingValue = "local",
+        matchIfMissing = true
+)
+@RequiredArgsConstructor
+public class LocalFileDownloader implements FileDownloader {
+
+    @Override
+    public FileDownloadContent download(FileInfo fileInfo) {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
