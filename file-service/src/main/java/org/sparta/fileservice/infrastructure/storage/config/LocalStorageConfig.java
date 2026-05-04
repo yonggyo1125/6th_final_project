8aa5177c8102d1e8783522fe6746016718d14746
yonggyo1981 <yonggyo00@kakao.com>
2026-05-04 17:55:57 +0900

    .

--- file-service/src/main/java/org/sparta/fileservice/infrastructure/storage/config/LocalStorageConfig.java
+++ file-service/src/main/java/org/sparta/fileservice/infrastructure/storage/config/LocalStorageConfig.java
@@ -2,9 +2,12 @@ package org.sparta.fileservice.infrastructure.storage.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 @Configuration
 @ConditionalOnProperty(
@@ -13,10 +16,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         matchIfMissing = true
 )
 @RequiredArgsConstructor
+@EnableConfigurationProperties(LocalStorageProperties.class)
 public class LocalStorageConfig implements WebMvcConfigurer {
+
+    private final LocalStorageProperties properties;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String handlerPattern = properties.getFormattedUrl() + "**";
+        String rawPath = properties.path();
+        String absolutePath = new File(rawPath).getAbsolutePath();
 
+        if (!absolutePath.endsWith(java.io.File.separator)) {
+            absolutePath += java.io.File.separator;
+        }
+
+        registry.addResourceHandler( handlerPattern)
+                .addResourceLocations("file:" + absolutePath)
+                .setCachePeriod(3600);
     }
 }
