11a11ef49d0871c54a479f276f02ee609868c036
yonggyo1981 <yonggyo00@kakao.com>
2026-05-10 21:40:38 +0900

    .

--- file-service/src/main/java/org/sparta/fileservice/application/query/FileQueryResult.java
+++ file-service/src/main/java/org/sparta/fileservice/application/query/FileQueryResult.java
@@ -1,5 +1,36 @@
 package org.sparta.fileservice.application.query;
 
-public record FileQueryResult(
+import lombok.Builder;
+import org.sparta.fileservice.domain.FileInfo;
 
-) {}
+import java.time.LocalDateTime;
+
+@Builder
+public record FileQueryResult(
+    Long id,
+    String groupId,
+    String tagName,
+    String fileName,
+    String contentType,
+    String extension,
+    boolean isImage,
+    boolean isVideo,
+    String filePath,
+    String fileUrl,
+    LocalDateTime createdAt
+) {
+    public static FileQueryResult from(FileInfo fileInfo) {
+        return FileQueryResult.builder()
+                .id(fileInfo.getId())
+                .groupId(fileInfo.getGroup().getGroupId())
+                .tagName(fileInfo.getGroup().getTag().name())
+                .fileName(fileInfo.getMetadata().getFileName())
+                .contentType(fileInfo.getMetadata().getContentType())
+                .extension(fileInfo.getMetadata().getExtension())
+                .isImage(fileInfo.getMetadata().isImage())
+                .isVideo(fileInfo.getMetadata().isVideo())
+                .filePath(fileInfo.getFilePath())
+                .createdAt(fileInfo.getCreatedAt())
+                .build();
+    }
+}
