91ab50deeda34c4c33952eecad658a4be00d01f2
yonggyo1981 <yonggyo00@kakao.com>
2026-05-11 08:11:57 +0900

    .

--- file-service/src/main/java/org/sparta/fileservice/infrastructure/persistence/config/JPAConfig.java
+++ file-service/src/main/java/org/sparta/fileservice/infrastructure/persistence/config/JPAConfig.java
@@ -4,8 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JPAConfig {
     @PersistenceContext
