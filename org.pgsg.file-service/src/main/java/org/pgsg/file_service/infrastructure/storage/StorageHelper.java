8a1129d661949e891a3fd170b03847f300ed5c31
yonggyo1981 <yonggyo00@kakao.com>
2026-05-14 15:21:58 +0900

    .

--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/StorageHelper.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/StorageHelper.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class StorageHelper {
 
     public static String getStorageFileName(String fileName) {
-        String ext =  fileName.lastIndexOf(".") != -1 ? fileName.substring(0, fileName.lastIndexOf(".") + 1) : "";
+        String ext =  fileName.lastIndexOf(".") != -1 ? fileName.substring(fileName.lastIndexOf(".") + 1) : "";
 
         return UUID.randomUUID().toString() + (StringUtils.hasText(ext) ? "." + ext : "");
     }
