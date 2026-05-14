b58fcd60d6d377b0633ecf6c811ae78c9880c1e7
yonggyo1981 <yonggyo00@kakao.com>
2026-05-14 15:43:25 +0900

    .

--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/presentation/FileController.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/presentation/FileController.java
@@ -1,13 +1,39 @@
 package org.pgsg.file_service.presentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.pgsg.file_service.application.FileService;
+import org.pgsg.file_service.presentation.dto.FileRequest;
+import org.pgsg.file_service.presentation.dto.FileResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class FileController {
+    private final FileService fileService;
 
+    @PostMapping(path = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<FileResponse.Upload> upload(@Valid FileRequest.Upload request, @RequestPart("file") MultipartFile[] files) throws IOException {
+        List<FileResponse.Upload> uploads = new ArrayList<>();
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) continue;
+
+            UUID fileId = fileService.upload(request.toServiceDto(file));
+            uploads.add(new FileResponse.Upload(fileId));
+        }
+
+        return uploads;
+    }
 
 }
--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/presentation/dto/FileRequest.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/presentation/dto/FileRequest.java
@@ -3,6 +3,10 @@ package org.pgsg.file_service.presentation.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.pgsg.file_service.application.dto.FileServiceDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FileRequest {
@@ -12,5 +16,16 @@ public class FileRequest {
 
             @NotBlank(message = "tag는 필수 입력값입니다.")
             String tag
-    ) {}
+    ) {
+        public FileServiceDto.FileUpload toServiceDto(MultipartFile file) throws IOException {
+            return FileServiceDto.FileUpload.builder()
+                    .groupId(this.groupId)
+                    .tag(this.tag)
+                    .inputStream(file.getInputStream())
+                    .originalFileName(file.getOriginalFilename())
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
+        }
+    }
 }
