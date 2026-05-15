0d582116b34b2f20d431a20fc0d2a7c222a3a908
yonggyo1981 <yonggyo00@kakao.com>
2026-05-15 12:51:01 +0900

    .

--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/S3FileUploader.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/S3FileUploader.java
@@ -4,9 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pgsg.file_service.domain.FileInfo;
 import org.pgsg.file_service.domain.FileTag;
+import org.pgsg.file_service.domain.exception.FileStorageException;
 import org.pgsg.file_service.domain.service.FileUploader;
+import org.pgsg.file_service.infrastructure.storage.config.S3StorageProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Component
@@ -15,9 +25,33 @@ import org.springframework.stereotype.Component;
         havingValue = "s3"
 )
 @RequiredArgsConstructor
+@EnableConfigurationProperties(S3StorageProperties.class)
 public class S3FileUploader implements FileUploader {
+
+    private final S3Client s3Client;
+    private final S3StorageProperties properties;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
     @Override
     public String upload(FileTag tag, FileInfo.FileSource source) {
-        return "";
+        String today = formatter.format(LocalDate.now());
+        String storageFileName = StorageHelper.getStorageFileName(source.originalFileName());
+        String relativePath = "%s/%s/%s".formatted(tag.getDirectory(), today, storageFileName);
+
+        try (InputStream inputStream = source.inputStream()) {
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(properties.bucket())
+                    .key(relativePath)
+                    .contentType(source.contentType())
+                    .build();
+
+            // S3 저장소 업로드
+            s3Client.putObject(request, RequestBody.fromInputStream(inputStream, source.contentLength()));
+
+            return relativePath;
+        } catch (Exception e) {
+            log.error("AWS S3 파일 업로드 실패 - 사유: {}", e.getMessage(), e);
+            throw new FileStorageException("파일 업로드 중 오류가 발생했습니다.");
+        }
     }
 }
--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/config/S3StorageConfig.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/storage/config/S3StorageConfig.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Slf4j
 @Configuration
@@ -17,4 +22,17 @@ import org.springframework.context.annotation.Configuration;
 public class S3StorageConfig {
 
     private final S3StorageProperties properties;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(
+                properties.accessKey(),
+                properties.secretKey()
+        );
+
+        return S3Client.builder()
+                .region(Region.of(properties.region()))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
 }
