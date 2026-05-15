b81311502214483336c4a1aa95b67397da6047d0
yonggyo1981 <yonggyo00@kakao.com>
2026-05-15 11:07:50 +0900

    .

--- org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/security/SecurityRoleChecker.java
+++ org.pgsg.file-service/src/main/java/org/pgsg/file_service/infrastructure/security/SecurityRoleChecker.java
@@ -1,5 +1,7 @@
 package org.pgsg.file_service.infrastructure.security;
 
+import org.pgsg.common.util.SecurityUtil;
+import org.pgsg.config.security.UserDetailsImpl;
 import org.pgsg.file_service.domain.FileInfo;
 import org.pgsg.file_service.domain.service.RoleChecker;
 import org.springframework.stereotype.Component;
@@ -10,21 +12,30 @@ import java.util.UUID;
 public class SecurityRoleChecker implements RoleChecker {
     @Override
     public boolean isMaster() {
-        return true;
+        return isLoggedIn() && getLoggedUser() != null &&
+                getLoggedUser().getUserRole().contains("MASTER");
     }
 
     @Override
     public boolean isMine(FileInfo fileInfo) {
-        return true;
+
+        return getLoggedUserId() != null &&
+                fileInfo.getCreatedBy() != null &&
+                fileInfo.getCreatedBy().equals(getLoggedUserId());
     }
 
     @Override
     public boolean isLoggedIn() {
-        return true;
+
+        return getLoggedUser() != null;
     }
 
     @Override
     public UUID getLoggedUserId() {
-        return UUID.randomUUID();
+        return SecurityUtil.getCurrentUserId().orElse(null);
+    }
+
+    private UserDetailsImpl getLoggedUser() {
+        return SecurityUtil.getCurrentUser().orElse(null);
     }
 }
